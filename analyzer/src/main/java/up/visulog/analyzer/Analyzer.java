0d6eacb1f3b34900b09aa3fcee9536f0a296503e
Marie-Colette <soline@corm.eu>
2021-10-01 22:49:49 +0200

    Numérotation des todos de Analyser.java

--- analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
+++ analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
@@ -26,14 +26,14 @@ public class Analyzer {
             plugin.ifPresent(plugins::add);
         }
         // run all the plugins
-        // TODO: try running them in parallel
+        // TODO#1: try running them in parallel
         for (var plugin: plugins) plugin.run();
 
         // store the results together in an AnalyzerResult instance and return it
         return new AnalyzerResult(plugins.stream().map(AnalyzerPlugin::getResult).collect(Collectors.toList()));
     }
 
-    // TODO: find a way so that the list of plugins is not hardcoded in this factory
+    // TODO#2: find a way so that the list of plugins is not hardcoded in this factory
     private Optional<AnalyzerPlugin> makePlugin(String pluginName, PluginConfig pluginConfig) {
         switch (pluginName) {
             case "countCommits" : return Optional.of(new CountCommitsPerAuthorPlugin(config));
