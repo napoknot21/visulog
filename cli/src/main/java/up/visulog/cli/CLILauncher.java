a29d51dda7378919174969566b0ad9ac1a2b5d32
lucasi <ivan.lucasreiner@gmail.com>
2021-10-11 19:44:25 +0200

    Ajout de commentaires dans CLILauncher.java

--- cli/src/main/java/up/visulog/cli/CLILauncher.java
+++ cli/src/main/java/up/visulog/cli/CLILauncher.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public class CLILauncher {
 
     public static void main(String[] args) {
-        var config = makeConfigFromCommandLineArgs(args);
+        var config = makeConfigFromCommandLineArgs(args);  //création de la configuration en fonction des arguments choisis (voir fonction ci dessous)
         if (config.isPresent()) {
             var analyzer = new Analyzer(config.get());
             var results = analyzer.computeResults();
@@ -19,23 +19,24 @@ public class CLILauncher {
         } else displayHelpAndExit();
     }
 
-    static Optional<Configuration> makeConfigFromCommandLineArgs(String[] args) {
-        var gitPath = FileSystems.getDefault().getPath(".");
-        var plugins = new HashMap<String, PluginConfig>();
+    static Optional<Configuration> makeConfigFromCommandLineArgs(String[] args) { //reçoit les arguments passés en ligne de commande
+        var gitPath = FileSystems.getDefault().getPath("."); //cree une variable qui contient le chemin vers ce fichier
+        var plugins = new HashMap<String, PluginConfig>(); //cree une hashmap avec pour cles des Strings et pour valeur des "PluginConfig" (-> à definir dans PluginConfig.java)
+        /*Une hashmap est une sorte de liste qui associe à chaque valeur une clé qui permet de la retrouver facilement (bien plus pratique que les listes vues en L1) */
         for (var arg : args) {
-            if (arg.startsWith("--")) {
-                String[] parts = arg.split("=");
-                if (parts.length != 2) return Optional.empty();
+            if (arg.startsWith("--")) { //verifie que les arguments sont au format "--nomArg=valArg"
+                String[] parts = arg.split("="); //separe chaque argument en 2: le nom de l'argument (ex: "--addPlugin") et sa valeur ("ex: countCommits"), et les met dans un tableau
+                if (parts.length != 2) return Optional.empty(); //renvoie une valeur vide s'il manque le nom ou la valeur de l'argument
                 else {
                     String pName = parts[0];
                     String pValue = parts[1];
-                    switch (pName) {
+                    switch (pName) { //sépare les différents cas de figure en fonction du nom de l'argument
                         case "--addPlugin":
                             // TODO#1: parse argument and make an instance of PluginConfig
 
                             // Let's just trivially do this, before the TODO is fixed:
 
-                            if (pValue.equals("countCommits")) plugins.put("countCommits", new PluginConfig() {
+                            if (pValue.equals("countCommits")) plugins.put("countCommits", new PluginConfig() { //si l'argument a pour valeur "countCommits", crée un
                             });
 
                             break;
@@ -46,17 +47,17 @@ public class CLILauncher {
                             // TODO#3 (save command line options to a file instead of running the analysis)
                             break;
                         default:
-                            return Optional.empty();
+                            return Optional.empty(); //renvoie une valeur vide si le nom de l'argument n'est pas valide
                     }
                 }
             } else {
-                gitPath = FileSystems.getDefault().getPath(arg);
+                gitPath = FileSystems.getDefault().getPath(arg); //si l'argument n'est pas au format "--nomArg=valeurArg", donne à la variable gitPath le chemin donne par l'argument
             }
         }
-        return Optional.of(new Configuration(gitPath, plugins));
+        return Optional.of(new Configuration(gitPath, plugins)); //renvoie une configuration si c'est possible (si un plugin a bien ete defini)
     }
 
-    private static void displayHelpAndExit() {
+    private static void displayHelpAndExit() { //liste les noms d'arguments valables (et leurs valeurs?) et arrete le programme
         System.out.println("Wrong command...");
         //TODO#4: print the list of options and their syntax
         System.exit(0);
