524f9fc2fb4393d0422175c89d9d6db382c11191
lucasi <ivan.lucasreiner@gmail.com>
2021-10-11 20:47:40 +0200

    Ajout de commentaires dans AnalyzerResult.java

--- analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
+++ analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
@@ -14,11 +14,18 @@ public class AnalyzerResult { //classe qui contient les resultats obtenus via An
     }
 
     @Override
-    public String toString() { //renvoie les resultats en tant que chaîne de caractère
+    public String toString() { //renvoie les resultats en tant que String
         return subResults.stream().map(AnalyzerPlugin.Result::getResultAsString).reduce("", (acc, cur) -> acc + "\n" + cur);
     }
 
     public String toHTML() { //renvoie les resultats dans un format HTML
         return "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur) + "</body></html>";
     }
+
+    /*Informations un peu plus détaillées sur ce que renvoient les méthodes toString() et toHTML():
+    subResults : résultats obtenus via Analyzer sous forme de liste
+    stream() : renvoie subResults en tant que sequential stream (=flux sequentiel)
+    map() : applique la fontion entre () à chaque element du stream (ici la fonction ecrit chaque element au format String ou HTML)
+    reduce() : combine les resultats pour ne renvoyer qu'une seule ligne à la fin
+     */
 }
