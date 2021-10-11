e5177730a7efc96ee35c5fbd48099a8cbee603c9
lucasi <ivan.lucasreiner@gmail.com>
2021-10-11 20:23:32 +0200

    Ajout de commentaires dans AnalyzerResult.java

--- analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
+++ analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
@@ -2,23 +2,23 @@ package up.visulog.analyzer;
 
 import java.util.List;
 
-public class AnalyzerResult {
+public class AnalyzerResult { //classe qui contient les resultats obtenus via Analyzer sous forme de liste
     public List<AnalyzerPlugin.Result> getSubResults() {
         return subResults;
-    }
+    } //renvoie la liste des resultats
 
-    private final List<AnalyzerPlugin.Result> subResults;
+    private final List<AnalyzerPlugin.Result> subResults; //liste des resultats
 
-    public AnalyzerResult(List<AnalyzerPlugin.Result> subResults) {
+    public AnalyzerResult(List<AnalyzerPlugin.Result> subResults) { //constructeur de la classe
         this.subResults = subResults;
     }
 
     @Override
-    public String toString() {
+    public String toString() { //renvoie les resultats en tant que chaîne de caractère
         return subResults.stream().map(AnalyzerPlugin.Result::getResultAsString).reduce("", (acc, cur) -> acc + "\n" + cur);
     }
 
-    public String toHTML() {
+    public String toHTML() { //renvoie les resultats dans un format HTML
         return "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur) + "</body></html>";
     }
 }
