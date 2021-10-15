50048c7e11161a3c85f658f0e7ae6f2d9b20201b
garnierk <kevin_garnier@outlook.fr>
2021-10-15 19:34:51 +0200

    Ajout de commentaires

--- gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
@@ -1,37 +1,37 @@
 package up.visulog.gitrawdata;
 
 public class CommitBuilder {
-    private final String id;
-    private String author;
-    private String date;
-    private String description;
-    private String mergedFrom;
+    private final String id;    //identifiant du commit
+    private String author;      //auteur du commit
+    private String date;        //date de publication du commit
+    private String description; //Description du commit
+    private String mergedFrom;  //indique la branche dont provient le commit
 
     public CommitBuilder(String id) {
         this.id = id;
-    }
+    }   //Constructeur à partir de l'id
 
-    public CommitBuilder setAuthor(String author) {
+    public CommitBuilder setAuthor(String author) { //setter
         this.author = author;
         return this;
     }
 
-    public CommitBuilder setDate(String date) {
+    public CommitBuilder setDate(String date) {//setter
         this.date = date;
         return this;
     }
 
-    public CommitBuilder setDescription(String description) {
+    public CommitBuilder setDescription(String description) {//setter
         this.description = description;
         return this;
     }
 
-    public CommitBuilder setMergedFrom(String mergedFrom) {
+    public CommitBuilder setMergedFrom(String mergedFrom) {//setter
         this.mergedFrom = mergedFrom;
         return this;
     }
 
-    public Commit createCommit() {
+    public Commit createCommit() { //Cree un commit avec le build du commit
         return new Commit(id, author, date, description, mergedFrom);
     }
 }
