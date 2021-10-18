1b84ce0e5387d443aaf649f037e9ff136ef28c52
Jacqueline <JacquelineYang13@gmail.com>
2021-10-18 02:29:10 +0200

    Modification du type d'attribut de date et ajout d'un nouvel setter setDate qui prend en argument un string

--- gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
@@ -1,9 +1,14 @@
 package up.visulog.gitrawdata;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class CommitBuilder {
     private final String id;    //identifiant du commit
     private String author;      //auteur du commit
-    private String date;        //date de publication du commit
+    private Date date;        //date de publication du commit
     private String description; //Description du commit
     private String mergedFrom;  //indique la branche dont provient le commit
 
@@ -16,10 +21,21 @@ public class CommitBuilder {
         return this;
     }
 
-    public CommitBuilder setDate(String date) {//setter
+    public CommitBuilder setDate(Date date) {//setter
         this.date = date;
         return this;
     }
+    public CommitBuilder setDate(String dateSt) { //convertit un String en Sate, setter
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z",Locale.ENGLISH).parse(dateSt);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.date = date;
+        return this;
+    }
+
 
     public CommitBuilder setDescription(String description) {//setter
         this.description = description;
