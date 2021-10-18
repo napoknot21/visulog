1a1373f0b2ad1c12d06ff6ff52dd841c98cc3354
POUILLART lucas <lucas.pouillart1@gmail.com>
2021-10-18 20:52:57 +0200

    Update Configuration.java

--- config/src/main/java/up/visulog/config/Configuration.java
+++ config/src/main/java/up/visulog/config/Configuration.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class Configuration {
+public class Configuration { //Classe pour associer un chemin à un (ou plusieurs) Plugin(s) configurés d'une certaine manière
 
-    private final Path gitPath;
-    private final Map<String, PluginConfig> plugins;
+    private final Path gitPath; //Chemin pour le plugin
+    private final Map<String, PluginConfig> plugins; //Le(s) plugin(s) en question, organisé(s) dans une Map (associé à une chaîne de caractères)
 
     public Configuration(Path gitPath, Map<String, PluginConfig> plugins) {
         this.gitPath = gitPath;
         this.plugins = Map.copyOf(plugins);
     }
 
-    public Path getGitPath() {
+    public Path getGitPath() { //Un simple getter qui permet de récupérer le chemin associé au plugin
         return gitPath;
     }
 
-    public Map<String, PluginConfig> getPluginConfigs() {
+    public Map<String, PluginConfig> getPluginConfigs() { //Pareil pour récupérer les plugins configurés
         return plugins;
     }
 }
