853253a25c49748981d7b5345c0bbccbedc980eb
garnierk <kevin_garnier@outlook.fr>
2021-10-30 21:15:07 +0200

    #41 Patch2: Changement factorisation + adaptation à Commit.java et MergedCommit.java

--- gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/CommitBuilder.java
@@ -1,12 +1,64 @@
 package up.visulog.gitrawdata;
 
-public class CommitBuilder extends Builder {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CommitBuilder {
+    private final String id;    //identifiant du commit
+    private String author;      //auteur du commit
+    private Date date;        //date de publication du commit
+    private String description; //Description du commit
+    // dont provient le commit
 
     public CommitBuilder(String id) {
-        super(id);
+        this.id = id;
     }   //Constructeur à partir de l'id
 
+    public String getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public CommitBuilder setAuthor(String author) { //setter
+        this.author = author;
+        return this;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public CommitBuilder setDate(Date date) {//setter
+        this.date = date;
+        return this;
+    }
+
+    public CommitBuilder setDate(String dateSt) { //convertit un String en Sate, setter
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z", Locale.ENGLISH).parse(dateSt);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.date = date;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public CommitBuilder setDescription(String description) {//setter
+        this.description = description;
+        return this;
+    }
+
     public Commit createCommit() { //Cree un commit avec le build du commit
-        return new Commit(getId(), getAuthor(), getDate(), getDescription(), getMergedFrom());
+        return new Commit(getId(), getAuthor(), getDate(), getDescription());
     }
 }
