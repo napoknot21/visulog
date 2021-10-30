c106f2d8902592f6c17bc33320a673ccd138cb01
garnierk <kevin_garnier@outlook.fr>
2021-10-30 21:19:45 +0200

    #41 Patch4: Modification MergedcommitBuilder.java pour l'adapter à MergeCommit et Commit

--- gitrawdata/src/main/java/up/visulog/gitrawdata/MergeCommitBuilder.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/MergeCommitBuilder.java
@@ -1,11 +1,22 @@
 package up.visulog.gitrawdata;
 
-public class MergeCommitBuilder extends Builder {
+public class MergeCommitBuilder extends CommitBuilder {
+    private String mergedFrom;  //indique le dernier commit (commit parent) de la branche
 
     public MergeCommitBuilder(String id) {
         super(id);
     }
-    /*public MergeCommit createMergeCommit() { //Cree un commit avec le build du commit
+
+    public String getMergedFrom() {
+        return mergedFrom;
+    }
+
+    public CommitBuilder setMergedFrom(String mergedFrom) {//setter
+        this.mergedFrom = mergedFrom;
+        return this;
+    }
+
+    /*public MergeCommit createCommit() { //Cree un commit avec le build du commit
         return new MergeCommit(getId(), getAuthor(), getDate(), getDescription(),getMergedFrom());
     }TODO: Decommenter la methode lorsque la classe MergeCommit sera crée */
 }
