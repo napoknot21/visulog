1ec84854351d297bbf8fa599c15777a754d7130d
Marie-Colette <soline@corm.eu>
2021-11-04 12:29:59 +0100

    Création de CountMergeCommitsPerAuthorPlugin

---
+++ analyzer/src/main/java/up/visulog/analyzer/CountMergeCommitsPerAuthorPlugin.java
@@ -0,0 +1,34 @@
+package up.visulog.analyzer;
+
+import up.visulog.config.Configuration;
+import up.visulog.gitrawdata.Commit;
+import up.visulog.gitrawdata.MergeCommit;
+
+import java.util.List;
+
+public class CountMergeCommitsPerAuthorPlugin extends CountCommitsPerAuthorPlugin{
+
+
+    public CountMergeCommitsPerAuthorPlugin(Configuration generalConfiguration) {
+        super(generalConfiguration);
+    }
+
+    static Result processLog(List<Commit> gitLog) {
+        var result = new Result(); /* Crée un HashMap qui va associer auteur(key) à nb de commit(value) */
+
+        /*Parcous les commits*/
+        for (var commit : gitLog) {
+            /*Cherche dans result si "commit.author" est déjà associé à un nb de commit:
+            si c'est le cas renvoie le nb de commit
+            sinon renvoie 0 */
+            if(commit instanceof MergeCommit) {
+                var nb = result.commitsPerAuthor.getOrDefault(commit.author, 0);
+
+                /* met à jour le nb de commit avec put (remplace la valeur précédente associée à la clé)
+                 * si la clé y est déjà  */
+                result.commitsPerAuthor.put(commit.author, nb + 1);
+            }
+        }
+        return result;
+    }
+}
