1d7a87c963108e512a7e5977f6a507e6c1325153
napoknot21 <napoknot21@protonmail.com>
2021-11-09 10:36:12 +0100

    Implementation parseDiff

--- gitrawdata/src/main/java/up/visulog/gitrawdata/CharacterChanges.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/CharacterChanges.java
@@ -2,24 +2,44 @@ package up.visulog.gitrawdata;
 
 import java.io.BufferedReader;
 import java.nio.file.Path;
-import java.util.Date;
+import java.util.Date;String id, String author, Date date, String description,
+import java.io.IOException;
+import java.util.Optional;
 
 public class CharacterChanges extends ChangesDescription{
     public final int addedCharacters;
     public final int removedCharcaters;
+    public Commit commit;
 
-    public CharacterChanges(String id, String author, Date date, String description, int added, int removed) { // simplement le constructeur
-        super(id, author, date, description);
+    public CharacterChanges(int added, int removed, Commit commit) { // simplement le constructeur
+        super(commit.id, commit.author, commit.date, commit.description);
         this.addedCharacters = added;
         this.removedCharcaters = removed;
+        this.commit = commit;
     }
 
-    public static void/*CharacterChanges*/ parseDiffFromCommand (Path gitPath, String idCommit) {
-        //return parseDiff(ChangesDescription.processCommand ("git","diff -numstat " + idCommit,gitPath));
-        parseDiff(ChangesDescription.processCommand ("git","diff -numstat " + idCommit,gitPath));
+    public static CharacterChanges parseDiffFromCommand (Path gitPath, Commit commit) {
+        return parseDiff(ChangesDescription.processCommand ("git","diff --numstat " + commit.id,gitPath), commit);
     }
 
-    public static void/*CharacterChanges*/ parseDiff(BufferedReader reader){
-        System.out.println("reader");
+    public static CharacterChanges parseDiff(BufferedReader reader, Commit commit){
+        String line = null;
+        try {
+            line = reader.readLine();
+
+            int char_added = 0;
+            int char_deleted = 0;
+            while (line != null) {
+                String[] charChanged = line.split(" ");
+                char_added += Integer.valueOf(charChanged[0]);
+                char_deleted += Integer.valueOf(charChanged[1]);
+            }
+            CharacterChanges cc = new CharacterChanges(char_added,char_deleted,commit);
+            return cc;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
