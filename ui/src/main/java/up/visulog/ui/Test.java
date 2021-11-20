31a83c7391b5d9e254881defdd069e181e66ae2c
garnierk <kevin_garnier@outlook.fr>
2021-11-20 14:00:41 +0100

    Fix du test de l'ui

--- ui/src/main/java/up/visulog/ui/Test.java
+++ ui/src/main/java/up/visulog/ui/Test.java
@@ -18,6 +18,23 @@ public class Test extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Pane p = new Pane();
+        ArrayList<MethodButton> buttons = new ArrayList<>();
+        for (String key : MethodButton.NAME_TO_PLUGIN_NAME.keySet()) {
+            MethodButton b = new MethodButton(key);
+            b.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    b.toHtml(b.run());
+                }
+            });
+            buttons.add(b);
+        }
+        p.getChildren().addAll(buttons);
+        Scene scene = new Scene(p);
+        primaryStage.setTitle("Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 }
