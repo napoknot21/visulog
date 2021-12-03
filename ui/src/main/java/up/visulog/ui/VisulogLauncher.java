053c010c13201c8a8137134aa674b8b5eebb63fc
garnierk <kevin_garnier@outlook.fr>
2021-12-03 20:21:18 +0100

    #79 Changement de nom + Thread-Safe

--- ui/src/main/java/up/visulog/ui/VisulogLauncher.java
+++ ui/src/main/java/up/visulog/ui/VisulogLauncher.java
@@ -4,16 +4,17 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import up.visulog.ui.views.View;
 
-public class VisulogGUI extends Application {
+import java.awt.*;
+
+public class VisulogLauncher extends Application {
     private View view;
 
     public static void main(String[] args) {
         if (args.length != 0) return;
         //Todo: A modifier si on veut ajouter l'execution de l'ui via un plugin specifique
-        launch(args);
+        EventQueue.invokeLater(() -> launch(args));
     }
 
-    //Todo : rendre l'interface thread-safe
     @Override
     public void start(Stage primaryStage) {
         view = new View();
