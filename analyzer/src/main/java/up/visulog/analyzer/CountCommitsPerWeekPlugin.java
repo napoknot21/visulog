7cbb48407d29929be53b98c36ea131e5c6991370
lucasi <ivan.lucasreiner@gmail.com>
2021-12-24 18:14:20 +0100

    Suite implémentation CountCommitsPerWeek (prend désormais en compte les différences d'années, mais les semaines ne sont pas encore ordonnées)

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerWeekPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerWeekPlugin.java
@@ -17,19 +17,23 @@ public class CountCommitsPerWeekPlugin implements AnalyzerPlugin{
         for (var commit : gitLog){
             Calendar calendar = Calendar.getInstance(Locale.FRANCE);
             calendar.setTime(commit.date);
+            int year = calendar.get(calendar.YEAR);
             int weekNumber = calendar.get(Calendar.WEEK_OF_YEAR);
-            var nb = result.commitsPerWeek.getOrDefault(Integer.toString(weekNumber), 0);
-            result.commitsPerWeek.put(Integer.toString(weekNumber), nb + 1);
+            String keyDate = weekNumber + "_" + year;
+            var nb = result.commitsPerWeek.getOrDefault(keyDate, 0);
+            result.commitsPerWeek.put(keyDate, nb + 1);
         }
         return result;
     }
 
-    public static HashMap<String, Integer> trierHashMap(HashMap<String, Integer> map){
+    public static HashMap<String, Integer> trierHashMap(HashMap<String, Integer> map, int i){
         List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(map.entrySet());
         Collections.sort( list, new Comparator<Map.Entry<String, Integer>>(){
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                if  (Integer.valueOf(o1.getKey()) == Integer.valueOf(o2.getKey())) return 0;
-                if  (Integer.valueOf(o1.getKey()) > Integer.valueOf(o2.getKey())) return 1;
+                String[] key1 = o1.getKey().split("_");
+                String[] key2 = o2.getKey().split("_");
+                if  (key1[i] == key2[i]) return 0;
+                if  (Integer.valueOf(key1[i]) > Integer.valueOf(key2[i])) return 1;
                 else return -1;
             }
         });
@@ -66,15 +70,26 @@ public class CountCommitsPerWeekPlugin implements AnalyzerPlugin{
 
 
         public String getResultAsHtmlDiv() {
-            HashMap<String, Integer> sortedMap = trierHashMap(commitsPerWeek);
+            HashMap<String, HashMap<String, Integer>> annees = new HashMap<>();
+            for (var item : commitsPerWeek.entrySet()){
+                String[] date = item.getKey().split("_");
+                if (!annees.containsKey(date[1])){
+                    HashMap<String, Integer> commitsAnnee = new HashMap<>();
+                    annees.put(date[1], commitsAnnee);
+                }annees.get(date[1]).put(date[0], item.getValue());
+            }
+            System.out.println();
             StringBuilder html = new StringBuilder("<div>Commits per period : <ul>");
-            for (var item : sortedMap.entrySet()) {
-                html.append("<li>")
-                        .append(item.getKey())
-                        .append(": ")
-                        .append(item.getValue())
-                        .append("</ul>")
-                        .append("</li>");
+            for (var annee : annees.entrySet()) {
+                html.append(annee.getKey());
+                for (var item : annee.getValue().entrySet()) {
+                    html.append("<li>")
+                            .append(item.getKey())
+                            .append(": ")
+                            .append(item.getValue())
+                            .append("</ul>")
+                            .append("</li>");
+                }
             }
             html.append("</ul></div>");
             return html.toString();
