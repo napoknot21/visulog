629aacc5342072700ee2ea917798707e01381501
garnierk <kevin_garnier@outlook.fr>
2022-01-13 23:26:52 +0100

    Debut d'ajout des filtres sur la gui (bug actuellement present)

--- ui/src/main/java/up/visulog/ui/model/MapRelations.java
+++ ui/src/main/java/up/visulog/ui/model/MapRelations.java
@@ -2,9 +2,13 @@ package up.visulog.ui.model;
 
 import up.visulog.config.PluginConfig;
 
-import java.util.HashMap;
+import java.util.*;
 
 public interface MapRelations {
+    GregorianCalendar GREGORIAN_CALENDAR = new GregorianCalendar();
+    int CURRENT_YEAR = GREGORIAN_CALENDAR.get(Calendar.YEAR) - 1900;
+    int CURRENT_MONTH = GREGORIAN_CALENDAR.get(Calendar.MONTH)+1;
+    int CURRENT_DAY = GREGORIAN_CALENDAR.get(Calendar.DAY_OF_MONTH);
     HashMap<String, String> BUTTON_NAME_TO_PLUGIN_NAME = initButtonNameToPluginName();
     //Map (Nom du bouton -> nom du plugin)
     HashMap<String, String> RADIO_BUTTON_NAME = initializeRadioButtonName();
@@ -13,7 +17,6 @@ public interface MapRelations {
     // Map (Nom sur l'UI -> nom du plugin)
     HashMap<String, PluginConfig> PLUGINS = initializePlugins();
     //Map (Nom du plugin -> pluginConfig)
-
 
     private static HashMap<String, String> initializeNameToPluginName() { //Initialise la map automatiquement en fonction
         HashMap<String, String> NAME_TO_PLUGIN_NAME                     //En fonction des boutons
@@ -27,11 +30,30 @@ public interface MapRelations {
     }
 
     private static HashMap<String, String> initializeRadioButtonName() { //initialise la map
+        GregorianCalendar calendar = new GregorianCalendar();
         HashMap<String, String> RADIO_BUTTON_NAME = new HashMap<>();
-        RADIO_BUTTON_NAME.put("Par jour", "PerDay");
+        RADIO_BUTTON_NAME.put("Par defaut", createDate(GREGORIAN_CALENDAR));
+        calendar.add(Calendar.DAY_OF_MONTH,-1);
+        RADIO_BUTTON_NAME.put("Sur le dernier jour", createDate(calendar));
+        calendar.add(Calendar.DAY_OF_MONTH,-6);
+        RADIO_BUTTON_NAME.put("Sur la derniere semaine", createDate(calendar));
+        calendar.add(Calendar.DAY_OF_MONTH,-7);
+        RADIO_BUTTON_NAME.put("Sur les 14 derniers jours", createDate(calendar));
+        calendar = (GregorianCalendar) GREGORIAN_CALENDAR.clone();
+        calendar.add(Calendar.MONTH,-1);
+        RADIO_BUTTON_NAME.put("Sur le dernier mois",createDate(calendar));
         //Todo: a remplir selon les filtres
 
         return RADIO_BUTTON_NAME;
+    }
+
+
+    private static String createDate(int year, int month, int day) {
+        return "PerPeriod-"+year+'/'+(month+1)+'/'+day+"-"+MapRelations.CURRENT_YEAR+'/'+MapRelations.CURRENT_MONTH+'/'+MapRelations.CURRENT_DAY;
+    }
+
+    private static String createDate (Calendar calendar) {
+        return createDate(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
     }
 
     private static HashMap<String, String> initButtonNameToPluginName() { //Initialise la map
@@ -49,7 +71,9 @@ public interface MapRelations {
         HashMap<String, PluginConfig> PLUGINS = new HashMap<>();
 
         for (String name : NAME_TO_PLUGIN_NAME.values()) {
-            PLUGINS.put(name, new PluginConfig());
+            String[] args = name.split("-");
+            if (args.length != 3) PLUGINS.put(name,new PluginConfig());
+            else PLUGINS.put(args[0], new PluginConfig(args[1],args[2]));
         }
         return PLUGINS;
     }
