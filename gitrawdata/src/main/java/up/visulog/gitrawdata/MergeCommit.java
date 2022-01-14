e15c6f0acc03bcbf53fe093e538ca77001c3abe8
napoknot21 <napoknot21@protonmail.com>
2022-01-15 00:24:07 +0100

    JavaDoc #2

--- gitrawdata/src/main/java/up/visulog/gitrawdata/MergeCommit.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/MergeCommit.java
@@ -4,13 +4,26 @@ import java.math.BigInteger;
 import java.util.Date;
 
 public class MergeCommit extends Commit{
-    public final String mergedFrom; // la branche d'où vient la modification
 
+    public final String mergedFrom;
+
+    /**
+     * Constructeur de MergeCommit
+     * @param id L'identifiant en hexadecimal d'après API
+     * @param author Le nom du collaborateur qui a commit
+     * @param date Le moment auquel le commit a été réalisé
+     * @param description Une brève description de la modification qui a été faite
+     * @param mergedFrom La branche d'où vient la modification
+     */
     public MergeCommit(BigInteger id, String author, Date date, String description, String mergedFrom) { // Constructeur
         super(id, author, date, description);
         this.mergedFrom = mergedFrom;
     }
 
+    /**
+     * @Override de toString()
+     * @return la chaine de caractères des caractéristiques du commit
+     */
     @Override
     public String toString() { // la méthode pour afficher les caractéristiques d'un commit
         return "Commit{" +
